67b5c0c482585672cee11ce323351f8f8e316277
Ishan <Braviaspan2002@gmail.com>
2023-11-30 23:41:04 +0530

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,13 @@ function App() {
   
   return (
     <>
-      <div className='bg-slate-300 w-[100vw] h-[100vh] flex justify-center items-center'>
+      <div className='bg-slate-300 w-[100vw] h-[calc(100vh-4rem)] flex justify-center items-center'>
         <Hamburger/>
+      </div>
+      <div className='h-[4rem] bg-slate-300'>
+        <div className='w-[95%] mx-auto border-t-2 border-t-black flex justify-center '> 
+        <p className='text-xl mt-3'>View the source code on <a href='https://github.com/JollyBolt/Animated-Hamburger-1' className='underline font-bold'>Github</a> and star the repository. </p>
+        </div>
       </div>
     </>
   )
